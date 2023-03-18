4dad369a11eb158dc661ac47465b01b5bd226206
imane <rkhisimane02@gmail.com>
2023-03-18 06:56:16 +0100

    some changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -134,12 +134,12 @@
                                         </div>
                                     </div>
                                 </div>
-								@endif
+                            @endif
                         </div>
                     </div>
                 </div>
                 <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
-                    
+
                     <div class="post d-flex flex-column-fluid" id="kt_post">
                         <div id="kt_content_container" class="container-xxl">
 
